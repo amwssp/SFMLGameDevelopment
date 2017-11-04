1b3aaed606d843391e128f44eb7df5f500718580
Andrew <amwssp@gmail.com>
2017-11-04 08:58:18 -0400

    beginning ch3

---
+++ Aircraft.h
@@ -0,0 +1,19 @@
+//Aircraft.h
+
+#ifndef AIRCRAFT_H
+#define AIRCRAFT_H
+
+#include "Entity.h"
+
+class Aircraft : public Entity {
+ public:
+  enum Type { Eagle, Raptor, };
+
+ public:
+  explicit Aircraft (Type type);
+
+ private:
+  Type mType;
+};
+
+#endif
---
+++ Entity.h
@@ -0,0 +1,18 @@
+//Entity.h
+
+#ifndef ENTITY_H
+#define ENTITY_H
+
+#include <SFML/Graphics.hpp>
+
+class Entity {
+ public:
+  void setVelocity(sf::Vector2f velocity);
+  void setVelocity(float vx, float vy);
+  sf::Vector2f getVelocity() const;
+
+ private:
+  sf::Vector2f mVelocity;
+};
+
+#endif
