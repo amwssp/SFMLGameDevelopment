2d9668221785a721f05bf523231406654e86e343
Andrew <amwssp@gmail.com>
2018-01-25 12:32:01 -0500

    StateStack definition added

--- ch5/StateStack.h
+++ ch5/StateStack.h
@@ -1,11 +1,41 @@
 #ifndef STATESTACK_H
 #define STATESTACK_H
 
-class StateStack {
- public:
-  void pushState(States::ID id);
-  void popState();
-  void clearStates();
+#include <SFML/System/NonCopyable.hpp>
+#include <SFML/System/Time.hpp>
+
+#include "State.h"
+#include "StateIdentifiers.h"
+
+class StateStack private sf::NonCopyable {
+public:
+	enum Action {Push, Pop, Clear};
+
+public:
+	explicit StateStack(State::Context context);
+
+	template <typename T>
+	void registerState(States::ID stateID);
+
+	void update(sf::Time dt);
+	void draw();
+	void handleEvent(const sf::Event& event);
+
+	void pushState(States::ID stateID);
+	void popState();
+	void clearStates();
+
+	bool isEmpty() const;
+
+private:
+	State::Ptr createState(States::ID stateID);
+	void applyPendingChanges();
+
+private:
+	std::vector<State::Ptr> mStack;
+	std::vector<PendingChange> mPendingList;
+	State::Context mContext;
+	std::map<States::ID, std::function<State::Ptr()>> mFactories;
 };
 
 #endif
